c2ce05dd79312acdbc20352fc0443c8774513ba5
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-10-28 13:38:31 +0700

    Update bai3.2.py

--- Bai 3/bai3.2.py
+++ Bai 3/bai3.2.py
@@ -1,4 +1,10 @@
+print("Sinh vien:Vo Van Manh")
+print("Ma so SV:245752021610011")
+print("#########################")
+#########################
 def sum(a, b):
  return a+ b
 c = sum(4, 5);
-print("Tong cua 4 va 5 = " + str(c))
+print("Tong cua 4 va 5 = " + str(c))
+
+
