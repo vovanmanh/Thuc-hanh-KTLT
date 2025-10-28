776194f9de60f9e91e830b83b3420e6f365d0972
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-10-28 13:38:02 +0700

    Update bai3.1.py

--- Bai 3/bai3.1.py
+++ Bai 3/bai3.1.py
@@ -1,6 +1,11 @@
+print("Sinh vien:Vo Van Manh")
+print("Ma so SV:245752021610011")
+print("#########################")
+#########################
 def sum(a, b):
  print("sum = " + str(a + b))
 # tinh tong 2 so 4,5
 sum(4, 5)
 # tinh tong 2 so 3,7
 sum(3, 7)
+
