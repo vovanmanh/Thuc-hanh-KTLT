7b64d9ee4513a59002a4fb4a534a5f7cacfeee0d
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-10-28 13:39:41 +0700

    Update bai3.6.py

--- Bai 3/bai3.6.py
+++ Bai 3/bai3.6.py
@@ -1,3 +1,7 @@
+print("Sinh vien:Vo Van Manh")
+print("Ma so SV:245752021610011")
+print("#########################")
+#########################
 def get_sum(*num):
  tmp = 0
  # duyet cac tham so
@@ -5,4 +9,6 @@ def get_sum(*num):
    tmp += i
  return tmp
 result = get_sum(1, 2, 3, 4, 5)
-print(result)
+print(result)
+
+
