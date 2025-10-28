5f049065fe87f039e9fbef489652478dd68e3b6f
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-10-28 13:39:09 +0700

    Update bai3.4.py

--- Bai 3/bai3.4.py
+++ Bai 3/bai3.4.py
@@ -1,6 +1,12 @@
+print("Sinh vien:Vo Van Manh")
+print("Ma so SV:245752021610011")
+print("#########################")
+#########################
 a = "Hello Guy!"
 def say(a):
  a = "Vinh University"
  print(a)
 say(a)
-print(a)
+print(a)
+
+
