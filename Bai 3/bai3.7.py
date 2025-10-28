65dcee9b3bc6d616d778164a2e97fe9b5c2f2afd
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-10-28 13:39:58 +0700

    Update bai3.7.py

--- Bai 3/bai3.7.py
+++ Bai 3/bai3.7.py
@@ -1,6 +1,12 @@
+print("Sinh vien:Vo Van Manh")
+print("Ma so SV:245752021610011")
+print("#########################")
+#########################
 def checkValue(n):
  if n%2 == 0:
   print ("Đây là một số chẵn")
  else:
   print ("Đây là một số lẻ")
-checkValue(7)
+checkValue(7)
+
+
