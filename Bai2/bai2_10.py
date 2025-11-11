9ba1f06f64682499194272449d63727d8f928550
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-11-11 14:13:54 +0700

    Add files via upload

---
+++ Bai2/bai2_10.py
@@ -0,0 +1,9 @@
+print("Sinh vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+a="hi i am python programmer"
+b=a.split()
+print (b)
+c=" ".join(b)
+print(c)
