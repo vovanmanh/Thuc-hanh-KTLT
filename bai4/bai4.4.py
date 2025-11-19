e29c7c28fd97072026e8b82990f2ee2b7ccd6bed
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-11-19 23:19:59 +0700

    Add files via upload

---
+++ bai4/bai4.4.py
@@ -0,0 +1,8 @@
+print("Sinh Vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+ds = input('Danh sach:').split()
+print(ds)
+for so in ds:
+    print(so)
