c6cf6d3805c9ba26c3c906693585642244fc7334
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-11-19 23:49:21 +0700

    Add files via upload

---
+++ bai4/bai4.6.py
@@ -0,0 +1,13 @@
+print("Sinh vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+ho_ten_day_du = input("Nhap ho va ten:")
+
+cac_tu=ho_ten_day_du.split()
+
+ho=cac_tu[0]
+ten=cac_tu[-1]
+
+print(f"Ho:{ho}")
+print(f"Ten:{ten}")
