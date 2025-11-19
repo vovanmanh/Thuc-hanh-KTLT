6ec34f42ca8a530ab22da2ad8a2dcc467ef8a002
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-11-19 20:28:04 +0700

    Update bai3.8.py

--- Bai 3/bai3.8.py
+++ Bai 3/bai3.8.py
@@ -1,26 +1,35 @@
+
 print("Sinh vien:Vo Van Manh")
 print("Ma so SV:245752021610011")
 print("#########################")
 #########################
 import math
-pos = [0,0]
+
+pos = [0, 0]   
+
 while True:
- s = input()
- if not s:
-  break
- movement = s.split(" ")
- direction = movement[0]
- steps = int(movement[1])
- if direction=="UP":
-   pos[0]+=steps
- elif direction=="DOWN":
-   pos[0]-=steps
- elif direction=="LEFT":
-   pos[1]-=steps
- elif direction=="RIGHT":
-   pos[1]+=steps
- else:
-  pass
-######################
-print (int(round(math.sqrt(pos[1]**2+pos[0]**2))))
+    s = input()
+    if not s:
+        break
+    try:
+       movement = s.split()
+       direction = movement[0]
+       steps = int(movement[1])
+    except (IndexError,ValueError):
+          continue
+    if direction == "UP":
+        pos[0] += steps
+    elif direction == "DOWN":
+        pos[0] -= steps
+    elif direction == "LEFT":
+        pos[1] -= steps
+    elif direction == "RIGHT":
+        pos[1] += steps
+    else:
+        pass
+
+distance = math.sqrt(pos[0]**2 + pos[1]**2)
+print(round(distance))
+
+
 
