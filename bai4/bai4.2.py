d1b61b08e55b289c62e222b8a70e15003b213a89
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-11-19 22:55:18 +0700

    Add files via upload

---
+++ bai4/bai4.2.py
@@ -0,0 +1,10 @@
+print("Sinh Vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+S = input('Nhap chuoi:')
+
+for ch in S:
+    if ch == ' ' or ch == '\t':
+        continue
+    print(ch)
