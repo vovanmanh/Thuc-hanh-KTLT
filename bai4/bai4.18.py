ea5aa2d4018c85b51e5216c88875014f7beb6b7b
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-11-23 10:11:03 +0700

    Add files via upload

---
+++ bai4/bai4.18.py
@@ -0,0 +1,24 @@
+print("Sinh vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+def tao_list_fibonacci(n):
+    if n<= 0:
+        return[]
+    a,b = 0,1
+    ds_fib = []
+    
+    while a < n:
+        ds_fib.append(a)
+        a,b = b,a + b
+
+    return ds_fib
+
+try:
+    n = int(input("Nhap so nguyen n:"))
+    list_fib = tao_list_fibonacci(n)
+    print(list_fib)
+except ValueError:
+    print("Gia tri khong hop le.")
+
+    
