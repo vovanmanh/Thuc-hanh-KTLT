b19adf69efc0c3a5b5941c78ab0a211214f52997
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-11-24 15:22:19 +0700

    Add files via upload

---
+++ bai5/bai5.4.py
@@ -0,0 +1,12 @@
+print("Sinh Vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+import numpy as np
+array_original = np.arange(12, 38)
+array_reversed = array_original[::-1]
+
+print("Mảng được tạo:")
+print(array_original)
+print("\nMảng đảo ngược:")
+print(array_reversed)
