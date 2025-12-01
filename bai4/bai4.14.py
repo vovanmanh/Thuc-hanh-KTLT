b6f1603c41ceec299baae6b17489d5c7148d54db
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-12-02 01:27:05 +0700

    Add files via upload

---
+++ bai4/bai4.14.py
@@ -0,0 +1,14 @@
+print("Sinh Vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+ds = input('Nhap chuoi:').split()
+x = ds[0:-1]
+
+ds.append('abc')
+
+ds.remove('123')
+ds.sort()
+
+for ch in ds:
+    print(ch)
