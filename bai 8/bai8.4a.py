aaa6fb4a548171ceb454274467a691a279afe409
vovanmanh245752021610011 <manhvovan022@gmail.com>
2025-12-01 22:32:52 +0700

    Add files via upload

---
+++ bai 8/bai8.4a.py
@@ -0,0 +1,13 @@
+print("Sinh Vien:Vo Van Manh")
+print("MSV:245752021610011")
+print("###################")
+###################
+from tkinter import *
+
+window = Tk()
+window.title("Welcome to LikeGeeks app")
+
+
+window.geometry('350x200')
+
+window.mainloop()
